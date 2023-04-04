af2a647a50122c044c09e9689fe2e2fa5dec711f
saurabh <saurabhsingh.bba17@gmail.com>
2023-04-04 06:17:35 +0530

    configured dashboard with tailwind and integrated api of search and add user

--- components/AdminFlow/AddUserCardUtils.jsx
+++ components/AdminFlow/AddUserCardUtils.jsx
@@ -1,7 +1,9 @@
 import { Box, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 import { Avatar } from "@chakra-ui/react";
-function AddUserCardUtils({ el }) {
+function AddUserCardUtils({ data }) {
+  // console.log("data...........", data);
+  const AvatarSrc = data.profile_picture;
   return (
     <Box width={"400px"} pl={"5"}>
       {" "}
@@ -11,12 +13,15 @@ function AddUserCardUtils({ el }) {
           <Avatar
             size={"sm"}
             alt="userIcon"
-            src="https://tse4.mm.bing.net/th?id=OIP.fzSnClvueUiDCZNJINMWywHaEK&pid=Api&P=0"
+            src={
+              AvatarSrc ||
+              "https://tse4.mm.bing.net/th?id=OIP.fzSnClvueUiDCZNJINMWywHaEK&pid=Api&P=0https://tse4.mm.bing.net/th?id=OIP.fzSnClvueUiDCZNJINMWywHaEK&pid=Api&P=0"
+            }
           />
         </Box>
         <VStack alignItems={"left"}>
           <Text fontSize={"18px"} fontFamily={"Lato"}>
-            {el}
+            {data.first_name == "" ? data.username : data.first_name + " " + data.last_name}
           </Text>
           <Text fontSize={"12px"} color={"#A097BF"} fontFamily={"Lato"}>
             Player
